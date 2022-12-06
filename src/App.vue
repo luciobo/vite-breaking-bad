78bd30be247cffd1d465dea150d8e7ff245847aa
luciobo <luciobo@icloud.com>
2022-12-06 15:24:55 +0100

    commit quasi iniziale

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import TheHeader from "./components/TheHeader.vue"
+import TheMain from "./components/TheMain.vue"
+import TheFooter from "./components/TheFooter.vue"
+
+
+export default {
+    components: { TheHeader, TheMain, TheFooter, },
+
+    data() {
+        return {
+
+        };
+    },
+    methods: {},
+
+    beforeMount() { },
+}
+</script>
+
+
+<template>
+    <div class="">
+        <div>
+            <TheHeader></TheHeader>
+        </div>
+        <div>
+            <TheMain></TheMain>
+        </div>
+        <div>
+            <TheFooter></TheFooter>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
+
---
+++ src/components/TheMain.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="">
+        <div class="row mx-3 d-flex justify-content-between py-5">
+            <div class="col-2 d-flex align-items-center">
+                <img src="" alt="">
+                <span class="ps-2">
+                    <a class="text-light text-decoration-none small" href="">ciao a tutti</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    data() {
+        return {
+            
+        };
+    },
+    methods: {},
+    beforeMount() {},
+}
+</script>
+
+<style scoped>
+
+</style>
